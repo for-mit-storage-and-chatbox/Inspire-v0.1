43f213fa5b1ead9201d35b8430f119e3234495aa
Tharunraj Optimus :-) <59702671+tharunoptimus@users.noreply.github.com>
2020-09-01 12:42:14 +0530

    Delete filemanage.rb

--- filemanage.rb
+++
@@ -1,7 +0,0 @@
-//Please don't think this as an important file. This script will count two numbers which is the basis of this program
-
-def sum_eq_n?(arr, n)
-  return true if arr.empty? && n == 0
-
-  arr.product(arr).reject { |a,b| a == b }.any? { |a,b| a + b == n }
-end
